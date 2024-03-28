686b8a8c69afd8a20803eda1e8e58a8766863eb5
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-03-28 16:57:43 +0200

    Зміна назви класу інсталлера

---
+++ Assets/Scripts/Installers/StartGameSceneInstaller.cs
@@ -0,0 +1,16 @@
+using MainMenu;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+    public class StartGameSceneInstaller : MonoInstaller
+    {
+        [SerializeField] private MainMenuHandler _mainMenuHandler;
+    
+        public override void InstallBindings()
+        {
+            Container.Bind<MainMenuHandler>().FromInstance(_mainMenuHandler);
+        }
+    }
+}
