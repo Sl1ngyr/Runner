d2ff58c0f4bf91c5c338a245a8d7599be757b506
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-03-29 23:11:04 +0200

    Рефакторинг SceneLoader
    Відредаговано клас, та впроваджено ProjectContext.

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scene
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void TransitionToSceneByIndex(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+    }
+}
