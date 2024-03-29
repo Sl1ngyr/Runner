9c812bbb7fd67cd0f80a8497fad12b56fb4aaf7e
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-03-29 22:59:39 +0200

    Реалізовано виклик UI кінця гри та призупинення ігрових об'єктів

---
+++ Assets/Scripts/Player/EndGameHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class EndGameHandler : MonoBehaviour
+    {
+        public event Action onEndGameTriggered;
+        
+        private void EndDeathAnimation()
+        {
+            onEndGameTriggered?.Invoke();
+        }
+    }
+}
