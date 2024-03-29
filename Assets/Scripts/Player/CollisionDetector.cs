c99190d83dd8c2b65f4ea1d048a4fba8017db9dc
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-03-29 22:57:07 +0200

    Реалізація зіткнення з перешкодами

---
+++ Assets/Scripts/Player/CollisionDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class CollisionDetector : MonoBehaviour
+    {
+        public event Action onObstacleDetected;
+
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.gameObject.tag == "Obstacle")
+            {
+                onObstacleDetected?.Invoke();
+            }
+        }
+    }
+}
