b5b8a753e3355bc9544697c6029954d3f1a8351c
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-03-30 03:50:33 +0200

    Реалізовано меню кінця гри
    Реалізовано анімацію напису GameOver через DoTween
    Впроваджено кнопку рестарту левела

---
+++ Assets/Scripts/UI/EndGame/EndGameMenu.cs
@@ -0,0 +1,56 @@
+﻿using Ad;
+using DG.Tweening;
+using Player;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace UI.EndGame
+{
+    public class EndGameMenu : MonoBehaviour
+    {
+        [Header("Menu data")]
+        [SerializeField] private TextMeshProUGUI _endGameText;
+        [SerializeField] private GameObject _endGameMenuObject;
+        [SerializeField] private ReviveManager _reviveManager;
+        
+        [Space]
+        [Header("Animation text data")]
+        [SerializeField] private float duration;
+        [SerializeField] private float _fadeValue = 0;
+        [SerializeField] private float _appearValue = 1;
+        
+        [Inject] private EndGameHandler _endGameHandler;
+        
+        private void AnimationEndGameText()
+        {
+            var seq = DOTween.Sequence();
+            seq.Append(_endGameText.DOFade(_fadeValue, duration));
+            seq.Append(_endGameText.DOFade(_appearValue, duration));
+        }
+        
+        private void EnableEndGameMenu()
+        {
+            _endGameMenuObject.gameObject.SetActive(true);
+            AnimationEndGameText();
+        }
+
+        private void DisableEndGameMenu()
+        {
+            _endGameMenuObject.gameObject.SetActive(false);
+        }
+        
+        private void OnEnable()
+        {
+            _endGameHandler.onEndGameTriggered += EnableEndGameMenu;
+            _reviveManager.onAdToReviveCompleted += DisableEndGameMenu;
+        }
+
+        private void OnDisable()
+        {
+            _endGameHandler.onEndGameTriggered -= EnableEndGameMenu;
+            _reviveManager.onAdToReviveCompleted -= DisableEndGameMenu;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/UI/EndGame/EndGameMenuPanel.cs
@@ -0,0 +1,31 @@
+﻿using Scene;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI.EndGame
+{
+    public class EndGameMenuPanel : MonoBehaviour
+    {
+        [SerializeField] private Button _restartLevel;
+        [SerializeField] private int _gameSceneBuildIndex = 1;
+
+        [Inject] private SceneLoader _sceneLoader;
+
+        
+        private void RestartLevel()
+        {
+            _sceneLoader.TransitionToSceneByIndex(_gameSceneBuildIndex);
+        }
+        
+        private void OnEnable()
+        {
+            _restartLevel.onClick.AddListener(RestartLevel);
+        }
+
+        private void OnDisable()
+        {
+            _restartLevel.onClick.RemoveListener(RestartLevel);
+        }
+    }
+}
